eb112bac765bf79e024f5bdfd9f3a63d98b75bd9
nltesown <nlte@magic.fr>
2018-06-29 15:20:21 +0200

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import nodeResolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import buble from "rollup-plugin-buble";
+import uglify from "rollup-plugin-uglify";
+
+export default {
+  entry: "src/main.js",
+  dest: "dist/bundle.js",
+  plugins: [
+    nodeResolve({ jsnext: true }),
+    commonjs(),
+    buble(),
+    uglify()
+  ],
+  format: "iife",
+  sourceMap: true
+}
+
---
+++ src/js/main.js
@@ -0,0 +1,92 @@
+$(main);
+
+function main() {
+  var ctrl = new ScrollMagic.Controller();
+  var sections = _($("section"))
+    .map((section, i) => {
+      return {
+        id: $(section).data("id"),
+        elSection: section,
+        elSplash: section.querySelector(".splash"),
+        elOverlay: section.querySelector(".overlay"),
+        elTitle: section.querySelector(".title")
+      };
+    })
+    .value();
+
+  _(sections).forEach((section, i) => {
+    var sectionHeight = $(section.elSection).height();
+    new ScrollMagic.Scene({
+      triggerElement: section.elSection,
+      triggerHook: 0,
+      duration: sectionHeight
+    })
+      .setPin(section.elSplash, { pushFollowers: false })
+      .setTween(
+        new TimelineMax().add([
+          TweenMax.fromTo(
+            section.elOverlay,
+            1,
+            { opacity: 0.75 },
+            { opacity: 0.15, ease: Power4.easeOut }
+          ),
+          TweenMax.fromTo(
+            section.elTitle,
+            1,
+            { opacity: 0 },
+            { opacity: 1, ease: Power4.easeOut }
+          )
+        ])
+      )
+      // .addIndicators({
+      //   indent: 50,
+      //   colorStart: "red",
+      //   colorTrigger: "red",
+      //   colorEnd: "red"
+      // })
+      .on("leave", e => {})
+      .addTo(ctrl);
+  });
+
+  $(".title").each((i, title) => {
+    new ScrollMagic.Scene({
+      triggerHook: 0,
+      triggerElement: title,
+      offset: -24,
+      duration: title
+    })
+      .setPin(title, { pushFollowers: false })
+      // .addIndicators({
+      //   indent: 100,
+      //   colorStart: "pink",
+      //   colorTrigger: "pink",
+      //   colorEnd: "pink"
+      // })
+      .addTo(ctrl);
+  });
+
+  $(".info, .quote").each((i, quote) => {
+    new ScrollMagic.Scene({
+      triggerElement: quote,
+      triggerHook: 0.25,
+      duration: "50%"
+    })
+      .setTween(
+        new TimelineMax().add([
+          TweenMax.fromTo(
+            quote,
+            1,
+            { opacity: 1 },
+            { opacity: 0, ease: Power4.easeOut }
+          )
+        ])
+      )
+      // .addIndicators({
+      //   indent: 100,
+      //   colorStart: "yellow",
+      //   colorTrigger: "yellow",
+      //   colorEnd: "yellow"
+      // })
+      .addTo(ctrl);
+  });
+}
